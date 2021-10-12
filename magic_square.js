4614ece3d128b926b70563895a829e5e30d01139
Damian <dawnz1@hotmail.com>
2021-10-12 08:37:06 -0500

    Completed Magic 15 3x3 Matrix

--- magic_square.js
+++ magic_square.js
@@ -1,6 +1,9 @@
+let problems = document.getElementById("problems");
+
 function check(id) {
  let val =document.getElementById(id);
  let isValid=true;
+
  if (isNaN(val.value) || val.value < 1 || val.value > 9) {
     // text = "Input not valid";
     {
@@ -22,6 +25,7 @@ function check(id) {
 // console.log(id);
 
 }
+// console.log();
 
 function isValid() {
     let row1 = document.getElementsByClassName("row1");
@@ -30,9 +34,17 @@ function isValid() {
     let col1 = document.getElementsByClassName("col1");
     let col2 = document.getElementsByClassName("col2");
     let col3 = document.getElementsByClassName("col3");
-
+    problems.innerHTML ="";
     let ans1,ans2,ans3,ans4,ans5,ans6 =0;
     let check1,check2,check3, check4,check5,check6= false;
+    const numbers = Array.from(document.getElementsByClassName("val"));
+    const number = numbers.map((number)=>number.value);
+    if(new Set(number).size !== number.length)
+    {
+        problems.innerHTML ="Non Unique Values <br/>";
+    }
+// problems="yes";
+console.log(number);
     // console.log(document.getElementById(row1[1].firstElementChild.id).value);
     for(let i=0; i<row1.length; i++)
     {
@@ -129,6 +141,7 @@ function isValid() {
         if(check1 && check2 && check3 && check4 && check5 && check6)
         {
             console.log("valid combination for rows and columns");
+            problems.innerHTML +="valid combination for rows and columns <br/>";
             return true;
         }
         else
@@ -136,28 +149,34 @@ function isValid() {
                 if(!check1)
                 {
                     console.log("invalid combination for row 1",ans1);
+                    problems.innerHTML +="invalid combination for row 1 <br/>";
                 }
                 if(!check2)
                 {
                     console.log("invalid combination for row 2",ans2);
+                    problems.innerHTML +="invalid combination for row 2 <br/>";
                     
                 }
                 if(!check3)
                 {
                     console.log("invalid combination for row 3",ans3);
+                    problems.innerHTML +="invalid combination for row 3 <br/>";
                 }
                 if(!check4)
                 {
                     console.log("invalid combination for column 1",ans4);
+                    problems.innerHTML +="invalid combination for column 1 <br/>";
                 }
                 if(!check5)
                 {
                     console.log("invalid combination for column 2",ans5);
+                    problems.innerHTML +="invalid combination for column 2 <br/>";
                     
                 }
                 if(!check6)
                 {
                     console.log("invalid combination for column 3",ans6);
+                    problems.innerHTML +="invalid combination for column 3 <br/>";
                 }
                 return false;
             }
