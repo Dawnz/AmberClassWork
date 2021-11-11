5d2075a2dd1190d5e19436061c64e7f9428e397d
Damian <dawnz1@hotmail.com>
2021-11-11 10:36:18 -0500

    minor design changes and prettier adjustments

--- form_validator.js
+++ form_validator.js
@@ -1,32 +1,25 @@
-function validateForm(assignmentForm)   
-{
-    var messages = [];
-    if (document.forms["student-form"]["fname"].value=="")
-    {
-        messages.push("Please fill in your first name.");
-        
-    }
-    if (document.forms["student-form"]["lname"].value=="")
-    {
-        messages.push("Please fill in your last name.");
-    }
-    if (document.forms["student-form"]["email"].value=="")
-    {
-        messages.push("Please fill in your email address.");
-    }
-    if (document.forms["student-form"]["password"].value=="")
-    {
-        messages.push("Please fill in your password.");
-    }
-    if (document.forms["student-form"]["gender"].value=="first")
-    {
-        messages.push("Please select your Gender.");
-    }
-    if (messages.length > 0) {
-        alert(messages.join('\n'));
-        return false;
-    } else {
-        return true;
-        
-    }
+function validateForm(assignmentForm) {
+   //    console.log("working");
+   var messages = [];
+   if (document.forms["student-form"]["fname"]?.value == "") {
+      messages.push("Please fill in your first name.");
+   }
+   if (document.forms["student-form"]["lname"]?.value == "") {
+      messages.push("Please fill in your last name.");
+   }
+   if (document.forms["student-form"]["email"]?.value == "") {
+      messages.push("Please fill in your email address.");
+   }
+   if (document.forms["student-form"]["password"]?.value == "") {
+      messages.push("Please fill in your password.");
+   }
+   if (document.forms["student-form"]["gender"]?.value == "first") {
+      messages.push("Please select your Gender.");
+   }
+   if (messages.length > 0) {
+      alert(messages.join("\n"));
+      return false;
+   } else {
+      return true;
+   }
 }
--- vertical_nav.js
+++ vertical_nav.js
@@ -4,21 +4,17 @@ const title = document.querySelector("#header-text");
 console.log(title);
 // console.log(content);
 
-navbar.forEach((element,index) =>{
-    element.addEventListener('click',function(){
-        navbar.forEach(nav=>nav.classList.remove("active"));
-        this.classList.add("active");
-        title.innerHTML=this.firstElementChild.innerHTML;
-        // console.log(this.firstElementChild.innerHTML);
-        content.forEach(element=>{
-            // console.log(!element.classList.contains("hide")?element.id:null);
-            if(!element.classList.contains("hide"))
-            {
-                element.classList.add("hide");
-            }
-            
-        });
-        content[index].classList.remove("hide");
-        // console.log();
-    });
+navbar.forEach((element, index) => {
+   element.addEventListener("click", function () {
+      navbar.forEach((nav) => nav.classList.remove("active"));
+      this.classList.add("active");
+      title.innerHTML = this.firstElementChild.innerHTML;
+      // console.log(this.firstElementChild.innerHTML);
+      content.forEach((element) => {
+         if (!element.classList.contains("hide")) {
+            element.classList.add("hide");
+         }
+      });
+      content[index].classList.remove("hide");
+   });
 });
